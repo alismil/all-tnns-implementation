9175f7635b9742edf25c31c23faeb8c2a58a5f94
alismil <alisa-milchevskaya@hotmail.co.uk>
2024-04-09 00:47:07 +0100

    untied conv layers v1 and v2

--- all-tnns/model.py
+++ all-tnns/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 """
 6 layers, of which layers 1, 3, and 5 are followed by 2 by 2 pooling layers. 
@@ -19,7 +20,7 @@ A dropout of 0.2 is applied to all layers during training.
 #             return (odim_i / stride[i]) + 1
 
 #         return shape_each_dim(0), shape_each_dim(1)
-    
+
 #     def __init__(self, idim):
 #         super().__init__()
 #         self.conv1 = LocallyConnected2d(3, 384, 7)
@@ -64,76 +65,153 @@ A dropout of 0.2 is applied to all layers during training.
 
 
 class LocallyConnected2dV1(nn.Module):
-    def __init__(self, in_height, in_width, kernel_height, kernel_width, stride=1, bias=False):
+    def __init__(
+        self, in_height, in_width, kernel_height, kernel_width, stride=1, bias=False
+    ):
         super(LocallyConnected2dV1, self).__init__()
         assert kernel_height <= in_height
         assert kernel_width <= in_width
 
         self.kernel_width = kernel_width
         self.kernel_height = kernel_height
-        self.num_kernels_w = in_width - kernel_width + 1
-        self.num_kernels_h = in_height - kernel_height + 1
+        self.num_kernels_w = math.floor((in_width - kernel_width) / stride) + 1
+        self.num_kernels_h = math.floor((in_height - kernel_height) / stride) + 1
         num_kernels = self.num_kernels_w * self.num_kernels_h
-        self.weights = [nn.Parameter(torch.randn(kernel_height * kernel_width)) for _ in range(num_kernels)]
-        
+        self.weights = [
+            nn.Parameter(torch.randn(kernel_height * kernel_width))
+            for _ in range(num_kernels)
+        ]
+
     def forward(self, x: torch.Tensor):
 
         out = []
         for i, weight in enumerate(self.weights):
             start_i, start_j = i // self.num_kernels_w, i % self.num_kernels_w
-            in_patch = x[start_i:start_i + self.kernel_height, start_j:start_j + self.kernel_width]
+            in_patch = x[
+                start_i : start_i + self.kernel_height,
+                start_j : start_j + self.kernel_width,
+            ]
             in_patch = in_patch.reshape(self.kernel_height * self.kernel_width)
             out.append(in_patch.dot(weight))
 
         return torch.tensor(out).reshape(self.num_kernels_h, self.num_kernels_w)
-    
+
 
 class LocallyConnected2dV2(nn.Module):
-    def __init__(self, in_height, in_width, kernel_height, kernel_width, stride=1, bias=False):
+    def __init__(
+        self,
+        in_height,
+        in_width,
+        kernel_height,
+        kernel_width,
+        stride_h=1,
+        stride_w=1,
+        padding_h=0,
+        padding_w=0,
+        bias=False,
+    ):
         super(LocallyConnected2dV2, self).__init__()
-        assert kernel_height <= in_height
-        assert kernel_width <= in_width
+        assert kernel_height <= in_height + 2 * padding_h
+        assert kernel_width <= in_width + 2 * padding_w
 
         self.in_height = in_height
         self.in_width = in_width
-        self.num_kernels_w = in_width - kernel_width + 1
-        self.num_kernels_h = in_height - kernel_height + 1
-        num_kernels = self.num_kernels_w * self.num_kernels_h
+        self.stride_w = stride_w
+        self.stride_h = stride_h
+        self.padding_h = padding_h
+        self.padding_w = padding_w
+        self.kernel_width = kernel_width
+        self.kernel_height = kernel_height
+        self.in_height = in_height
+        self.in_width = in_width
+        self.num_kernels_w = (
+            math.floor((in_width - kernel_width + 2 * self.padding_w) / stride_w) + 1
+        )
+        self.num_kernels_h = (
+            math.floor((in_height - kernel_height + 2 * self.padding_h) / stride_h) + 1
+        )
+        self.num_kernels = self.num_kernels_w * self.num_kernels_h
 
-        weights = [nn.Parameter(torch.randn(kernel_height, kernel_width)) for _ in range(num_kernels)]
-        self.W = torch.zeros(in_height*in_width, num_kernels)
+        kernel_weights = [
+            nn.Parameter(torch.randn(kernel_height, kernel_width))
+            for _ in range(self.num_kernels)
+        ]
+        self.W = self._create_weight_matrix(kernel_weights)
 
+        if bias:
+            self.bias = nn.Parameter(
+                torch.randn(self.num_kernels_h, self.num_kernels_w)
+            )
+        else:
+            self.register_parameter("bias", None)
+
+    def _create_weight_matrix(self, weights):
+        init_w = torch.zeros(
+            (self.in_height + 2 * self.padding_h)
+            * (self.in_width + 2 * self.padding_w),
+            self.num_kernels,
+        )
         for i, weight in enumerate(weights):
-            start_i, start_j = i // self.num_kernels_w, i % self.num_kernels_w
-            for k in range(kernel_height):
-                begin = in_width * start_i + start_j
-                self.W[begin:begin+kernel_width, i] = weight[k,:]
+            start_i = (i // self.num_kernels_w) * self.stride_h
+            start_j = (i % self.num_kernels_w) * self.stride_w
+            for k in range(self.kernel_height):
+                begin = (self.in_width + 2 * self.padding_w) * start_i + start_j
+                init_w[begin : begin + self.kernel_width, i] = weight[k, :]
                 start_i += 1
-        
+
+        return init_w
+
     def forward(self, x: torch.Tensor):
         assert x.shape[0] == self.in_height
         assert x.shape[1] == self.in_width
 
-        x = x.view(self.in_height*self.in_width)
+        padding = (self.padding_w, self.padding_w, self.padding_h, self.padding_h)
+        x = F.pad(x, padding, "constant", 0)
+
+        x = x.view(
+            (self.in_height + 2 * self.padding_h) * (self.in_width + 2 * self.padding_w)
+        )
 
         out = x @ self.W
 
-        return out.reshape(self.num_kernels_h, self.num_kernels_w)
-    
-import time
-x = torch.rand(3,4)
+        out = out.reshape(self.num_kernels_h, self.num_kernels_w)
 
-lc2d1 = LocallyConnected2dV1(in_height=3, in_width=4, kernel_height=2, kernel_width=2)
-lc2d2 = LocallyConnected2dV2(in_height=3, in_width=4, kernel_height=2, kernel_width=2)
+        if self.bias is not None:
+            out += self.bias
 
-one = time.time()
+        return out
 
-lc2d1(x)
 
-two = time.time()
+# import time
+# x = torch.rand(3,4)
+
+# lc2d1 = LocallyConnected2dV1(in_height=3, in_width=4, kernel_height=2, kernel_width=2)
+# lc2d2 = LocallyConnected2dV2(in_height=3, in_width=4, kernel_height=2, kernel_width=2)
+
+# one = time.time()
+
+# lc2d1(x)
+
+# two = time.time()
+
+# lc2d2(x)
+
+# three = time.time()
+
+# print(two-one, three-two)
+
+
+x = torch.rand(3, 4)
+
+lc2d2 = LocallyConnected2dV2(
+    in_height=3,
+    in_width=4,
+    kernel_height=2,
+    kernel_width=2,
+    stride_h=3,
+    stride_w=3,
+    padding_h=1,
+    padding_w=2,
+)
 
 lc2d2(x)
-
-three = time.time()
-
-print(two-one, three-two)
